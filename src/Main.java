3b813cd448e76ad269acc0f0827c1e575bfabfa0
NATILEMMA <natilemma5@gmail.com>
2022-09-17 17:13:47 +0300

    added the hexagoncalculator and number digit adder

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 class Main{
   
   static Scanner input = new Scanner(System.in);
@@ -7,8 +6,35 @@ class Main{
 
     //twoNumbersCompare();
     //numberDigitAdder();
+    hexagonAreaCalculator();
   }
+  static void hexagonAreaCalculator(){
  
+
+  
+  // 34. Write a Java program to compute the area of a hexagon. Go to the editor
+  // Area of a hexagon = (6 * s^2)/(4*tan(π/6))
+  // where s is the length of a side
+  // Input Data:
+  // Input the length of a side of the hexagon: 6
+  // Expected Output
+  // The area of the hexagon is: 93.53074360871938
+
+    double lengthOfSide;
+    double AreaoFHexagon;
+    double PI =3.14;
+  
+  
+    System.out.println("Please insert the length of hexagon side");
+    lengthOfSide = input.nextInt();
+    input.close();
+
+    AreaoFHexagon =((6 * (Math.pow(lengthOfSide,2)))/(4*Math.tan(PI/6)));
+
+    System.out.println("The area of hexagon is: "+ AreaoFHexagon);
+  }
+
+
   static void numberDigitAdder(){
     int inputInteger;
     
