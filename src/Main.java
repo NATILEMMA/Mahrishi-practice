8a16fee475f6bc056060040abab249a99c9f099d
NATILEMMA <natilemma5@gmail.com>
2022-10-01 18:16:42 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ class Main{
 
   }
   public static void sortarray(){
-    int [] array = {1,4,5,6,4,8};
+    int [] array = {1,4,5,6,4,'8'};
     int max = 0;
 
     int length = array.length;
@@ -33,6 +33,7 @@ class Main{
           array[j] = max;
         }
       }
+      
     }
     for(int i = 0; i< length;i++){
       System.out.println(array[i]);
