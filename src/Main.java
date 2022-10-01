a24af95d7d89d23f415b80e723db045fff3e43f0
NATILEMMA <natilemma5@gmail.com>
2022-10-01 18:16:02 +0300

    added sort array

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,29 @@ class Main{
     //printOdd();
     //factor();
     //capitalizer();
-    beforeLastWord();
+    //beforeLastWord();
+      sortarray();
+
+  }
+  public static void sortarray(){
+    int [] array = {1,4,5,6,4,8};
+    int max = 0;
+
+    int length = array.length;
+    for(int i = 0 ; i < length; i++){
+      for (int j= 1 ; j<length;j++){
+        if(array[j-1]> array[j]){
+          max =  array[j-1];
+          array[j-1] = array[j];
+          array[j] = max;
+        }
+      }
+    }
+    for(int i = 0; i< length;i++){
+      System.out.println(array[i]);
+    }
+
+
   }
   public static void beforeLastWord(){
    
